3d4342502f4ddf3ea821ea2780bcd7dc8ce50ce0
“Chaminda1993” <c771904768@gmail.com>
2024-02-06 22:47:37 +0530

    update

--- setup.sh
+++ setup.sh
@@ -13,8 +13,8 @@ echo "Start pushing workflow to repository"
 git add .github/workflows/*.yml
 git add *.toml
 git add install_Company_dependencies.sh
-git commit -m "Seting up GitHub workflows"
-git push
+git commit -m "Seting up GitHub workflows" --no-verify
+git push --no-verify
 echo "Configuration successfully pushed to remote"
 else
 echo "Github actions workflows already setup in the project"
